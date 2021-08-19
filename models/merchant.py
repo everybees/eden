157c0a0d211d7a15deb45321e0d7de622b74a780
Love Okum <l.okum@semicolon.africa>
2021-08-19 17:54:54 +0100

    update Evolve project

--- models/merchant.py
+++ models/merchant.py
@@ -1,1 +1,11 @@
 # contains merchant class
+from models.User import user
+
+
+class Merchant(user):
+        def __init__(self,firstname, lastname, id, email, password, usertype, billing_address, card_details, shop):
+            super().__init__(firstname, lastname, id, email, password, usertype)
+
+            self.billing_address = billing_address
+            self.card_details = card_details
+            self.shop = shop
