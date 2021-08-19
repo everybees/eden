1ab896a82188d9c3c2694eab734c00ceaa11245e
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-08-19 20:04:50 +0100

    implemented functionality: can save user to database can read user from database

--- models/user.py
+++ models/user.py
@@ -1,2 +1,51 @@
-class User:
-    pass
+import abc
+
+from models.address import Address
+
+
+class User(abc.ABC):
+    def __init__(self, first_name, last_name, email, user_name, password):
+        self.__first_name: str = first_name
+        self.__last_name: str = last_name
+        self.__user_name: str = user_name
+        self.__email: str = email
+        self.__password: str = password
+        self.__home_address: Address
+        self.__phone_number: str
+
+    @property
+    def get__first_name(self):
+        return self.__first_name
+
+    def update_first_name(self, new_first_name: str):
+        self.__first_name = new_first_name
+
+    @property
+    def get__last_name(self):
+        return self.__last_name
+
+    def update__last_name(self, new_last_name: str):
+        self.__last_name = new_last_name
+
+    @property
+    def get__user_name(self):
+        return self.__user_name
+
+    def update_user_name(self, new_user_name: str):
+        self.__user_name = new_user_name
+
+    @property
+    def get__email(self):
+        return self.__email
+
+    def update__email(self, new_email: str):
+        self.__email = new_email
+
+    @property
+    def get__password(self):
+        return self.__password
+
+    def update__password(self, new_password: str):
+        self.__password = new_password
+
+
