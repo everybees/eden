44812c650ca64bfe53ce88db6a9d7f73d545e8d3
TobiStrings <ligalititobiloluwa2005@gmail.com>
2021-08-19 21:54:38 -0700

    Exceptions

--- models/Users/Exceptions.py
+++ models/Users/Exceptions.py
@@ -1,3 +1,8 @@
 class IncompleteDetails(Exception):
     def __init__(self, message):
         super(IncompleteDetails, self).__init__(message)
+
+
+class InvalidCustomer(Exception):
+    def __init__(self, message):
+        super(InvalidCustomer, self).__init__(message)
