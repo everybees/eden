af5dc1894485ae6a80183de6c5799dff86c19ae5
Daniel isiogugu <dannydix555@gmail.com>
2021-08-20 15:34:06 +0100

    updated Login

--- views/login.py
+++ views/login.py
@@ -1,4 +1,16 @@
-def login(email, password):
+def login_as_customer(email, password):
     email = email
     password = password
-    return "successful"
+    print("successful")
+
+
+def login_as_merchant(email, password):
+    email = email
+    password = password
+    print("successful")
+
+
+def login_as_admin(password):
+    admin_password = 1234
+    if admin_password == password:
+        print("successful")
