37944248d64ee5a20f637baae624cb283be8822b
TobiStrings <ligalititobiloluwa2005@gmail.com>
2021-08-26 09:47:07 -0700

    Merchant

--- models/Users/Merchants.py
+++ models/Users/Merchants.py
@@ -3,12 +3,12 @@ from models.Users.Users import User
 
 
 class Merchant(User):
-    def __init__(self, company_name, password, email,  company_reference_number):
+    def __init__(self, company_name, password, email, company_reference_number):
         super().__init__()
         self.company_name = company_name
         self.email = email
         self.password = password
-        self. company_reference_number = company_reference_number
+        self.company_reference_number = company_reference_number
 
     def set_merchant_password(self, password):
         self.password = password
@@ -29,12 +29,8 @@ class Merchant(User):
         self.company_name = company_name
 
     def display_company_reference_number(self):
-        return self.ompany_reference_number
+        return self.company_reference_number
 
     def register(self, company_name, email, password, reference_number):
-        if company_name =="" or email == "" or password =="" or reference_number =="":
+        if company_name == "" or email == "" or password == "" or reference_number == "":
             raise IncompleteDetails("Incomplete registration details")
-
-
-
-
