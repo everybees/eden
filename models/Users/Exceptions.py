97951f695dd569925b803d504166e43dd5c89a47
TobiStrings <ligalititobiloluwa2005@gmail.com>
2021-08-26 09:46:24 -0700

    Exception

--- models/Users/Exceptions.py
+++ models/Users/Exceptions.py
@@ -3,12 +3,7 @@ class IncompleteDetails(Exception):
         super(IncompleteDetails, self).__init__(message)
 
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
 class InvalidCustomer(Exception):
     def __init__(self, message):
         super(InvalidCustomer, self).__init__(message)
-=======
->>>>>>> Stashed changes
-=======
->>>>>>> Stashed changes
+
