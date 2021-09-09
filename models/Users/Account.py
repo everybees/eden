bac6e90391707213d53372e745f6cd0089b7fec1
TobiStrings <ligalititobiloluwa2005@gmail.com>
2021-09-09 14:01:34 -0700

    Account

--- models/Users/Account.py
+++ models/Users/Account.py
@@ -1,8 +1,13 @@
 from models.Users.Users import User
+from models.Users.address import Address
+import datetime
 
 
 class Account:
     user = User
+    address = Address
 
-    def __init__(self, user):
+    def __init__(self, user: User, address: Address):
         self.user = user
+        self.address = address
+        self.opening_date = datetime.datetime.now()
