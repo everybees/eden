a4100352eef63dd378e03f01580ce025e686c842
TobiStrings <ligalititobiloluwa2005@gmail.com>
2021-09-09 14:00:56 -0700

    Address

---
+++ models/Users/address.py
@@ -0,0 +1,6 @@
+class Address:
+    def __init__(self, street_name: str, street_number: str, city_name: str, state: str):
+        self.street_name = street_name
+        self.street_number = street_number
+        self.city_name = city_name
+        self.state = state
