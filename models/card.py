3f3711eb2458d7488e29e938db55bd9baece8908
Ehis Edemakhiota <edemaehiz@gmail.com>
2021-09-09 09:47:01 +0100

    added files

--- models/card.py
+++
@@ -1,8 +0,0 @@
-import enum
-
-
-class Card(enum.Enum):
-    MASTER_CARD = 1
-    VISA_CARD = 2
-    VERVE = 3
-    AMERICA_EXPRESS = 4
--- models/user_dto.py
+++
@@ -1,9 +0,0 @@
-
-from models.user import User
-
-
-class User_Dto(User):
-
-    def __init__(self, first_name="", last_name="", email="", user_name="", password=""):
-        super().__init__(first_name, last_name, email, user_name, password)
-
